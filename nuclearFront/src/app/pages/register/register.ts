56f7cfa13386aa9726051c712de048e5fc246bb7
juan támara <t4mara95@gmail.com>
2026-06-16 10:23:30 -0500

    modificaciones y arreglos de las vistas

--- nuclearFront/src/app/pages/register/register.ts
+++ nuclearFront/src/app/pages/register/register.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
+selector:'app-register',
+
+imports:[
+RouterLink
+],
+
+templateUrl:'./register.html',
+
+styleUrl:'./register.css'
 })
 export class Register {}
